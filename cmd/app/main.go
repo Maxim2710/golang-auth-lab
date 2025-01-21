f344a66ac2a57525010ecc629da5e58c12eb4f0d
PrimakMV <pm2710@mail.ru>
2025-01-21 14:26:09 +0300

    Minor improvements have been made

--- cmd/app/main.go
+++ cmd/app/main.go
@@ -2,10 +2,13 @@ package main
 
 import (
 	"github.com/Maxim2710/golang-auth-lab/internal/app"
+	"github.com/gin-gonic/gin"
 	"log"
 )
 
 func main() {
+	gin.SetMode(gin.DebugMode)
+
 	if err := app.Run(); err != nil {
 		log.Fatalf("Application terminated with error: %v", err)
 	}
