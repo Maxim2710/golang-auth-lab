7114353ee2aa92fd67ba298429b791128cda6b49
PrimakMV <pm2710@mail.ru>
2025-01-21 23:21:39 +0300

    Methods have been added to update the password, as well as to receive a user's email to the repository.

--- internal/database/repository/user_postgres.go
+++ internal/database/repository/user_postgres.go
@@ -19,3 +19,16 @@ func (r *UserRepository) GetUserById(id int) (*models.User, error) {
 	err := r.db.Get(&user, query, id)
 	return &user, err
 }
+
+func (r *UserRepository) GetUserByEmail(email string) (*models.User, error) {
+	var user models.User
+	query := `SELECT * FROM users WHERE email = $1`
+	err := r.db.Get(&user, query, email)
+	return &user, err
+}
+
+func (r *UserRepository) UpdatePasswordByEmail(email string, newPassword string) error {
+	query := `UPDATE users SET password = $1 WHERE email = $2`
+	_, err := r.db.Exec(query, newPassword, email)
+	return err
+}
