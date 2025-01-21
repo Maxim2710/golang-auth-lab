275aabb814d6d0f019c8558b6ef453a821859d6f
PrimakMV <pm2710@mail.ru>
2025-01-21 21:47:24 +0300

    Implemented the logic of token creation and validation

---
+++ pkg/utils/token.go
@@ -0,0 +1,41 @@
+package utils
+
+import (
+	"github.com/dgrijalva/jwt-go"
+	"time"
+)
+
+var jwtSecret = []byte("some_secret_key")
+
+type Claims struct {
+	Email string `json:"email"`
+	jwt.StandardClaims
+}
+
+func GenerateToken(email string) (string, error) {
+	expirationTime := time.Now().Add(12 * time.Hour)
+	claims := &Claims{
+		Email: email,
+		StandardClaims: jwt.StandardClaims{
+			ExpiresAt: expirationTime.Unix(),
+		},
+	}
+
+	token := jwt.NewWithClaims(jwt.SigningMethodHS256, claims)
+
+	return token.SignedString(jwtSecret)
+}
+
+func ValidateToken(tokenString string) (*Claims, error) {
+	claims := &Claims{}
+
+	token, err := jwt.ParseWithClaims(tokenString, claims, func(token *jwt.Token) (interface{}, error) {
+		return jwtSecret, nil
+	})
+
+	if err != nil || !token.Valid {
+		return nil, err
+	}
+
+	return claims, nil
+}
